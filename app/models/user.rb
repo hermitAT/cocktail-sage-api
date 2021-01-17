6f4614a4a1213c3166778dfb3a5d1bb4229a09c0
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2021-01-16 18:54:44 -0800

    Fixed users migration error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 
-  has_secure_password
+  #has_secure_password
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: {case_sensitive: false}
