9301f06ff4c0535c0fe31db6d0d7b63513db4171
hermitAT <ahthornev2@gmail.com>
2021-01-16 17:28:45 -0800

    Bugfix issue with recipe model validation

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,5 @@ class Recipe < ApplicationRecord
   belongs_to :flavour
   belongs_to :user, optional: true
   has_many :recipe_ingredients
-  
-  validates :name, :flavour_id, :user_id, :image_url, :summary, :instructions, presence: true
+
 end
