63d9f334f8b37914092b5d0f6f318d84cda523c1
ngampit <ngampitt@yahoo.com>
2021-01-17 19:45:28 +0000

    edited users routes

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,53 +1,57 @@
-class UsersController < Api::ApplicationController
-  def new
-    @user = User.new
-  end
+class Api::UsersController < Api::ApplicationController
+
 
   def create
-    @user = User.create(user_params)
+    @user = User.new(user_params)
+
     if @user.save
-      render :json {
-        user :user
-      }
+      render json: @user, status: :created, location: @user
     else
-      render json: {
-        error: "No such user; check the submitted email address"
-        status: 400
-      }, status: 400
+      render json: @user.errors, status: :unprocessable_entity
     end
   end
+  
 
-  def login(email, password)
-    user = User.find_by_email(email.downcase.stripe)
-    # If the user exists AND the password entered is correct.
-    if user && user.authenticate(password)
-      # Save the user id inside the browser cookie. This is how we keep the user 
-      # logged in when they navigate around our website.
+  def login
+    user = User.find_by_email(params[:email])
+    if user && user.password == params[:password]
+      
       render :json => {
-        user:user
+        user_name: user.name,
+        user_avatar: user.user_avatar
       }
     else
-      render json: {
+      render :json => {
         error: "No such user; check the submitted email address"
-        status: 400
-      }, status: 400
+      }
     end
+  
+  end
+
+  def index 
+    @users = User.all
+
+    render :json => {
+      users: @users
+    }
   end
 
   def show
     @user_recipes = User.find_by(params[:user_id]).recipes
+    @recipes_id = @user_recipes.select('id')
     @user_favorites = User.find_by(params[:user_id]).favorites
+    @favorites_id = @user_favorites.select('id')
 
 
-    render : json => {
-      user_recipes: @user_recipe
-      user_favorites: @user_favorites     
+    render :json => {
+      recipes_id: @recipes_id,
+      favorites_id: @favorites_id  
     }
   end 
 
-  private 
+  # private 
   
   def user_params
-    params.require(:user).permit(:name, :email, :password, :user_avartar)
+    params.require(:user).permit(:name, :email, :password, :user_url)
   end
 end
