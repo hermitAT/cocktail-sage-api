2243029b3221f9e9b305fcd6939c551ab740ff58
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2021-01-19 09:26:04 -0800

    Add comments to search function

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -5,28 +5,33 @@ module SearchHelper
     search_keys.delete('controller')
     search_keys.delete('action')
 
-    table_names = {
-      recipes: []
-    }
-
+    # Extract the name of a controller which called this function
     controller_name = params['controller'].split('/')[1]
 
+    # Initialize Object to store results for each search_key
     results = {}
 
+    # # RECIPES CONTROLLER # #
     if controller_name == 'recipes'
       
+      # Case-insensitive name
       if search_keys.include?('name')
         results['name'] = Recipe.where("LOWER (name) LIKE ?", "%#{params[:name]}%").ids
         search_keys.delete('name')
       end
+
+      # Ingredient or list of ingredients
       if search_keys.include?('ingredient_id')
         
+        # Tranform single ingredient to array or "string-array" to real array
+        # This is to be changed according to how Front-end sends arrays in query
         if params[:ingredient_id][0] == "["
           ingredient_ids = params[:ingredient_id].tr("[]",'').split(",")
         else
           ingredient_ids = [params[:ingredient_id]]
         end
 
+        # Perform search for each id in ingredients array (even if it's a single value)
         ingredient_ids.each { |id|
           results["ingredient_#{id}"] = RecipeIngredient.select(:recipe_id).where(ingredient_id: id).map { |recipe_ing|
             recipe_ing.recipe_id }
@@ -34,27 +39,38 @@ module SearchHelper
         search_keys.delete('ingredient_id')
       end
       
+      # General search for other keys
       search_keys.each { |key|
         results[key] = Recipe.where(key => params[key]).ids
       }
     end
 
-    
+    # INGREDIENTS CONTROLLER
     if controller_name == 'ingredients'
       search_keys.each { |key|
         results[key] = Ingredient.where(key => params[key]).ids 
       }
     end
-    #return results
+    
+    
+    # FORM STRICT/RELEVANT IDs arrays to return
+    
+    # Count occurence for each ID in the search results
     occurence = {}
     results.keys.each { |search_key|
       results[search_key].each { |id|
         occurence.has_key?(id) ? occurence[id] += 1 : occurence[id] = 1
       }
     }
+
+    # Sort IDs in occurence by value
     relevance = occurence.sort_by { |a, b| -b }.to_h.keys
+    
+    # Perform intersection operation over each array in search results
+    # to get IDs that common for all of them
     strict = results.values.reduce { |intersection, array| intersection & array }
 
+    # Return message if no results
     if relevance.length + strict.length == 0
       return 'No results found'
     else 
