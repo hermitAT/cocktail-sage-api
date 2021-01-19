925dec66f8e64a149a2460f99b5097b0677d4148
hermitAT <ahthornev2@gmail.com>
2021-01-18 22:08:55 -0800

    Fix endpoints for ratings/favorites, no longer requiring ids

--- config/routes.rb
+++ config/routes.rb
@@ -6,15 +6,20 @@ Rails.application.routes.draw do
     root to: 'application#index'
 
     resources :users, only: [:index, :show, :create]
-    post "users/login" => 'users#login'
-    get "ingredients/search" => 'ingredients#search'
+    post 'users/login' => 'users#login'
+    get 'ingredients/search' => 'ingredients#search'
     resources :ingredients, only: [:index, :show]
-    get "recipes/search" => 'recipes#search'
+    get 'recipes/search' => 'recipes#search'
     resources :recipes do
-      resources :ratings, only: [:create, :update, :destroy]
-      resources :favorites, only: [:create, :destroy]
+
+      post 'ratings' => 'ratings#create'
+      put 'ratings' => 'ratings#update'
+      delete 'ratings' => 'ratings#destroy'
+      
+      post 'favorites' => 'favorites#create'
+      delete 'favorites' => 'favorites#destroy'
+      
       resources :comments, only: [:create, :update, :destroy]
     end
-
   end
 end
