da134bb36a3da79b941ed55e403b59bffd30f61e
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2021-01-25 01:35:30 -0800

    return paginated my and favorite recipes for user

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -39,16 +39,27 @@ class Api::UsersController < Api::ApplicationController
   def show
 
     @recipes_id = Recipe.where(user_id: params[:id]).ids
+    @my_recipes = RecipeHelper.paginate(@recipes_id)
+    @my_recipes['current_page'] = RecipeHelper.get_recipes_full_data(@my_recipes[1])
+    @my_recipes['count'] = @recipes_id.length
+
     @favorites_id = Favorite.where(user_id: params[:id]).map {|entry| 
       entry[:recipe_id]
     }
+    @my_favorites = RecipeHelper.paginate(@favorites_id)
+    @my_favorites['current_page'] = RecipeHelper.get_recipes_full_data(@my_favorites[1])
+    @my_favorites['count'] = @favorites_id.length
+
     @user = User.select(:id, :name, :user_avatar, :created_at).find(params[:id])
 
 
     render :json => {
       user: @user,
       recipes_id: @recipes_id,
-      favorites_id: @favorites_id  
+      favorites_id: @favorites_id,
+      my_recipes: @my_recipes,
+      my_favorites: @my_favorites
+
     }
   end 
 
